1f63ada640792681e6ddd3157017d60c136a8e31
alexfmsu <alexfmsu@mail.ru>
2024-05-23 12:19:09 +0300

    grpc: grpc/unary/server/server.go

---
+++ grpc/unary/server/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	// system
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	// unary
+	loginpb "unary/proto/loginpb"
+
+	// external
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+var (
+	port string = "50050"
+)
+
+func (s *server) Login(ctx context.Context, req *loginpb.LoginRequest) (*loginpb.LoginResponse, error) {
+	if req.Login == "login" && req.Password == "passwd" {
+		res := loginpb.LoginResponse{Result: "Ok"}
+		fmt.Println("[server] (from client)", req)
+		return &res, nil
+	}
+
+	res := loginpb.LoginResponse{Result: "Error"}
+	return &res, nil
+}
+
+func main() {
+	listen, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen port %s: %v", port, err)
+		return
+	}
+	fmt.Println("[server] Listening port " + port + "...")
+
+	grpcServer := grpc.NewServer()
+
+	loginpb.RegisterLoginServiceServer(grpcServer, &server{})
+
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("Failed to register LoginServiceServer: %v", err)
+	}
+}
