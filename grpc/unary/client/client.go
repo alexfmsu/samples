a434f9cc8d825adbc0314c0d500dfec310556c59
alexfmsu <alexfmsu@mail.ru>
2024-05-23 12:19:25 +0300

    grpc: grpc/unary/client/client.go

---
+++ grpc/unary/client/client.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	// system
+	"context"
+	"fmt"
+	"log"
+
+	// unary
+	loginpb "unary/proto/loginpb"
+
+	// external
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	host string = "0.0.0.0"
+	port string = "50050"
+)
+
+func main() {
+	conn, err := grpc.NewClient(host+":"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to server: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	grpcClient := loginpb.NewLoginServiceClient(conn)
+
+	req := loginpb.LoginRequest{
+		Login:    "login",
+		Password: "passwd",
+	}
+
+	resp, err := grpcClient.Login(context.Background(), &req)
+	if err != nil {
+		log.Fatalf("Failed to login: %v", err)
+		return
+	}
+
+	fmt.Println("[from server]", resp)
+}
